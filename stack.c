d0f0668b55bedd759932ff8eeb60b22aaf693d04
Balaji Sundar <94917311+BalajiSundar28@users.noreply.github.com>
2023-01-13 21:13:41 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void push();
+void pop();
+void display();
+int ch, n = 5, ele, top = -1;
+
+void main() {
+	
+	int arr[n];
+	while (1) {
+		
+		printf ("1. insert\n2. delete\n3. display\n4. exit\nEnter your option : ");
+		scanf ("%d", &ch);
+		switch (ch) {
+			
+			case 1 :
+				push(arr);
+				break;
+			case 2 :
+				pop(arr);
+				break;
+			case 3 :
+				display(arr);
+				break;
+			case 4 :
+				exit(0);
+		}
+		printf ("\n\n");		
+	}
+}
+
+void push(int arr[]) {
+	
+	if (top == -1) {
+		
+		printf ("enter inserting element : ");
+		scanf ("%d", &ele);
+		top = 0;
+		arr[top] = ele;
+		printf ("\nelement inserted successfully :)");
+	}
+	else if (top == n-1) {
+		
+		printf ("\nStack is full");
+	}
+	else {
+		
+		printf ("enter inserting element : ");
+		scanf ("%d", &ele);
+		top++;
+		arr[top] = ele;
+		printf ("\nelement inserted successfully :)");
+	}
+}
+
+void pop(int arr[]) {
+	
+	if (top == -1) {
+		
+		printf ("\nStack is empty...");
+	}
+	else {
+		
+		printf ("Deleting element is : %d\n", arr[top]);
+		top--;
+		printf ("\nElement deleted successfully :)");
+	}
+}
+
+void display(int arr[]) {
+	
+	if (top == -1) {
+		
+		printf ("\nStack is empty...");
+	}
+	else {
+		
+		printf ("\nStack elements are...");
+		for (int i = 0; i < top+1; i++) {
+		
+			printf ("\n%d", arr[i]);
+		}	
+	}
+}
